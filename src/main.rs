e3127bc954c550c19c490c06b008d12fa79f7437
David Larson <dalarson@wpi.edu>
2019-10-02 13:02:53 -0400

    Can now read in boards from txt

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,61 @@
-const HEIGHT: usize = 100;
-const WIDTH: usize = 100;
+use std::fs;
+
+const HEIGHT: usize = 7;
+const WIDTH: usize = 10;
+type Point = (usize, usize);
 
 
 fn main() {
-    
-    let game_board = init_board();
-
+    let filepath = "src/input.txt".to_string();
+    let game_board = init_board_from_file(filepath);
+    print!("{:?}", game_board);
     print_board(game_board);
+
 
 }
 
-fn init_board() -> Vec<(usize, usize)> {
+/*
+ * init_board_from_file
+ */
+
+fn init_board_from_file(filepath: String) -> Vec<Point> {
+    let mut vec = init_board(); // get new sparse matrix instance
+    let contents = fs::read_to_string(filepath).expect("File not found!");
+
+    // maintain coordinates to populate sparse matrix
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+
+    for c in contents.chars() {
+        if c == '\n' { // new row
+            y += 1;
+            x = 0;
+        }
+        else if c == '1' {vec.push((x, y));} // add point to sparse matrix
+        x += 1;
+    }
+
+    // return newly created sparce matrix
+    return vec;
+}
+
+/*
+ * init_board()
+ * Initializes a new empty sparse array and returns it
+ */
+fn init_board() -> Vec<Point> {
     let vec = Vec::new();
-    
     return vec;
 }   
 
-fn print_board(board: Vec<(usize, usize)>) {
+fn print_board(board: Vec<Point>) {
     let mut blank_board: [[u8; HEIGHT]; WIDTH] = [[0; HEIGHT]; WIDTH];
     for i in board {
         blank_board[i.0][i.1] = 1;
     }
     for i in 0..HEIGHT {
         for j in 0..WIDTH {
-            print!("{}", if blank_board[i][j] == 0 {" "} else {"X"});
+            print!("{}", if blank_board[j][i] == 0 {" "} else {"X"});
         }
         print!("\n");
     }
